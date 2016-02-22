a29acdd03ba1c600b256e08f3251abfa015fac7f
Stefano Rossetti <stefano@webyourmind.com>
2016-02-23 00:33:03 +0100

    Refactor of csv file loading algorithm 1. loadfile/csvfile.controller.js: removed unnecessary functions and attributess 2. loadfile/upload-csv.html: removed old load button anf dile selector 3. services/fbData.service.js: added function saveSections and saveSection, modified  get in order to use section name to retrieve data

--- loadfile/csvfile.controller.js
+++ loadfile/csvfile.controller.js
@@ -1,65 +1,27 @@
-angular.module('myApp')
-.controller('CsvFileController', ['$scope', '$http', '$log','NgTableParams', 'FirebaseService', 'currentAuth', 'ParserService', function($scope, $http, $log, NgTableParams, FirebaseService, currentAuth, ParserService ){
-	//$log.debug("CsvFileController creato");
-    //$log.debug("CsvFileController currentAuth: ", currentAuth);
+angular.module('myApp').controller('CsvFileController', ['$log', 'FirebaseService', 'currentAuth', 'ParserService', function($log, FirebaseService, currentAuth, ParserService ){	
 
-	var CsvFileCtrl = this;
+	var CsvFileCtrl = this
 
-    CsvFileCtrl.currentAuth = currentAuth;
-    $log.debug("Current uid:", CsvFileCtrl.currentAuth.uid);
+    CsvFileCtrl.currentAuth = currentAuth
+    //$log.debug("Current uid:", CsvFileCtrl.currentAuth.uid);
 
-	CsvFileCtrl.csvcontent = "";
-	CsvFileCtrl.csvheader = true;
-	CsvFileCtrl.csvheaderVisible = true;
-	CsvFileCtrl.csvseparator = ",";
-	CsvFileCtrl.csvseparatorVisible = false;
-	CsvFileCtrl.csvresult = "";
-	CsvFileCtrl.csvaccept = ".csv";
-	CsvFileCtrl.showPreviewTable = false;
-	CsvFileCtrl.enableSaveBtn = false;
+    CsvFileCtrl.sections = []
+    CsvFileCtrl.content = ""
 
-	CsvFileCtrl.savedRecords = 0;
+    // 
+    //CsvFileCtrl.showContent = function($fileContent){
+     //   CsvFileCtrl.content = $fileContent
+    //};
 
-	// Watch su caricamento file CSV (innesca il reload automatico della pagina) 
-    var listener = $scope.$watch(
-		'CsvFileCtrl.csvresult',
-	    function(newValue, oldValue) {
-	        if (newValue){
-	        	//$log.debug("CsvFileCtrl.watch - CSV changed (csvresult):", newValue);                                   
-	    		CsvFileCtrl.tableParams = new NgTableParams(
-	    			{page:1,
-                     count: 100},
-	    			{data: CsvFileCtrl.csvresult}
-	    		); 
-	    		CsvFileCtrl.showPreviewTable = true;              	
-	    		CsvFileCtrl.enableSaveBtn = true;
-	        }else{
-	        	$log.debug("CsvFileCtrl.watch - no newval");                                 
-	        }
-	    }
-    );
-
-    $scope.$on("$destroy", function() {
-        if (listener) {
-            listener();
-        }
-    });
-
-    CsvFileCtrl.save = function(){
-    	CsvFileCtrl.enableSaveBtn = false;
-		CsvFileCtrl.savedRecords = FirebaseService.save(CsvFileCtrl.csvresult, CsvFileCtrl.currentAuth.uid);
-    }	
-
-     	
-    ////
-    CsvFileCtrl.sections = [];
-    CsvFileCtrl.content = "";
-    CsvFileCtrl.showContent = function($fileContent){
-        CsvFileCtrl.content = $fileContent;
-    };
-
+    // Read the CSV file content
     CsvFileCtrl.parseContent = function($fileContent){
-        CsvFileCtrl.content = $fileContent;
-        CsvFileCtrl.sections = ParserService.parse(CsvFileCtrl.content);
+        CsvFileCtrl.content = $fileContent
+        CsvFileCtrl.sections = ParserService.parse(CsvFileCtrl.content)
     };
+
+    // Send the data to Firebase backend
+    CsvFileCtrl.save = function(){
+    	$log.debug("CsvFileCtrl.save")  
+    	FirebaseService.saveSections(CsvFileCtrl.sections, CsvFileCtrl.currentAuth.uid)
+    }
 }])
--- services/fbData.service.js
+++ services/fbData.service.js
@@ -44,24 +44,48 @@ angular.module('myApp')
 		return addedRecords;
 	}
 
-	self.get = function(){
+	self.saveSections = function(sections, uid){	
+		_.each(sections, function(section, index, list){
+			$log.debug("FirebaseService.saveSections - Try to save section:", section.startLabel)
+			self.saveSection(section, uid)
+		})
+	}
+
+	self.saveSection = function(section, uid){	
+		// process the section name
+		var sectionName = section.startLabel.toLowerCase().replace(/ /g,'')		
+		//$log.debug("FirebaseService.saveSection - saving section: ", [sectionName, section.json])
+		var records = self.get(sectionName)
+		_.each(section.json, function(rec, index, list){
+			// add to the firebase 
+			records.$add(rec)
+		})
+	}
+
+	self.get = function(section){
+		if (section === undefined ){
+			section = 'sales';
+		}
+		$log.debug("FirebaseService.get section is:", section);
+		
 		if (self.uid === undefined ){
 			$log.debug("FirebaseService.get uid is undefined:", self.uid);
 			self.uid = UserAuthentication.getUid();
+			$log.debug("FirebaseService.get get the uid:", self.uid);
 		}else{
 			$log.debug("FirebaseService.get uid is defined:", self.uid);
 		}
-		if (self.records == undefined && self.uid != undefined){
+
+		//if (self.records == undefined && self.uid != undefined){
 			//$log.debug("FirebaseService.get [self.records] is undefined: get the values");
-			var endPoint = FBEndPoint + self.uid + "/" + "sales/";
-			$log.debug("FirebaseService.get endPoint:", endPoint);
+			var endPoint = FBEndPoint;
 			var ref = new Firebase(endPoint);
-			self.records = $firebaseArray(ref);			
+			self.records = $firebaseArray(ref.child(self.uid).child(section));			
 			return self.records;
-		}else {
+		//}else {
 			//$log.debug("FirebaseService.get [self.records] is defined: use the values");
-			return self.records;
-		}
+		//	return self.records;
+		//}
 	}
 
 	self.getLastTransactionId = function(records){
