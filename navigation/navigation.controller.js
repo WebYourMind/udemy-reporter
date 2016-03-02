32bf733c270223a23e3c065e84822fb5427c227f
Tony Iannelli <tony@webyourmind.com>
2016-03-02 21:19:57 +0000

    changed layout menus
    1 main.css: added new classes to customize layout
    2 index.html: added flex and removed class layout-xl
    3 main-wrapper.html: changed structure to split menu in toolbar and
    sidenav
    4 navigation.controller.js: added scope openLeftMenu and restored
    function changeTab/isActiveTab

--- navigation/navigation.controller.js
+++ navigation/navigation.controller.js
@@ -5,17 +5,21 @@ angular
    .controller('NavigationController', NavigationController);
 
 /* @ngInject */ 
-function NavigationController($scope, $log, Auth){
+function NavigationController($scope, $log, Auth, $mdSidenav){
 
-	/*$scope.item = 0;
+	$scope.item = 0;
 	$scope.changeTab = function(newItem){
 		$scope.item = newItem;
 	};
 	$scope.isActiveTab = function(item){
 		return $scope.item === item;
-	};*/
+	};
 
 	$scope.imagePath = 'http://placehold.it/350x150';
+
+	$scope.openLeftMenu = function() {
+		$mdSidenav('left').toggle();
+	};
 
     $scope.Auth = Auth
     $scope.Auth.$onAuth(function(result){
