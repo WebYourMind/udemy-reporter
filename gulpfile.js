60af91bab2f85928cb8bc7cc50957eb5224b1133
AlexWebYourmind <alex@webyourmind.com>
2016-03-13 17:07:41 +0000

    New GulpFile and Package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 var gulp = require('gulp');
-
-gulp.task('default', function() {
-	var browserSync = require('browser-sync');
-	var reload = browserSync.reload;
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 	// watch files for changes and reload
 	gulp.task('serve', function() {
-	  browserSync({
-	    server: {
-	      baseDir: '.'
-	    }
+	  browserSync.init({
+	    
+	       proxy: "udemy-reporter.localhost:8091",
+	       host:"udemy-reload",
+	       port: 8091,
+	       logLevel: "debug"
+	    
 	  });
 
 	  gulp.watch([	'*.html', 
 	  				'css/*.css', 
 	  				'authentication/*.js', 'authentication/*.html', 
-	  				'componenets/*.js', 'componenets/*.html', 
+	  				'components/*.js', 'components/*.html', 
 	  				'loadfile/*.js', 'loadfile/*.html',
 	  				'navigation/*.js', 'navigation/*.html',
 	  				'pages/*.html',
 	  				'reports/*.js', 'reports/*.html',
 	  				'services/*.js', 'services/*.html'], 
-	  			{cwd: '.'}, 
+	  			{cwd: './'}, 
 	  			reload);
 	});
-});
